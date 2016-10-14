fb118c138e5c632c252cbf4a2f68cc3f8da6ad70
Ivan Sadikov <isadikov@wynyardgroup.com>
2016-10-14 17:54:09 +1300

    address minor parquet-tools issues

--- parquet-tools/src/main/java/org/apache/parquet/tools/command/HeadCommand.java
+++ parquet-tools/src/main/java/org/apache/parquet/tools/command/HeadCommand.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -19,15 +19,24 @@
 package org.apache.parquet.tools.command;
 
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.PathFilter;
 
+import org.apache.parquet.hadoop.Footer;
+import org.apache.parquet.hadoop.ParquetFileReader;
 import org.apache.parquet.hadoop.ParquetReader;
+import org.apache.parquet.hadoop.metadata.ParquetMetadata;
 import org.apache.parquet.tools.Main;
+import org.apache.parquet.tools.json.JsonRecordFormatter;
 import org.apache.parquet.tools.read.SimpleReadSupport;
 import org.apache.parquet.tools.read.SimpleRecord;
 
@@ -42,11 +51,15 @@ public class HeadCommand extends ArgsOnlyCommand {
   public static final Options OPTIONS;
   static {
     OPTIONS = new Options();
-    Option help = OptionBuilder.withLongOpt("records")
+    Option recordsOpt = OptionBuilder.withLongOpt("records")
                                .withDescription("The number of records to show (default: " + DEFAULT + ")")
                                .hasOptionalArg()
                                .create('n');
-    OPTIONS.addOption(help);
+    Option jsonOpt = OptionBuilder.withLongOpt("json")
+                               .withDescription("Show records in JSON format.")
+                               .create('j');
+    OPTIONS.addOption(recordsOpt);
+    OPTIONS.addOption(jsonOpt);
   }
 
   public HeadCommand() {
@@ -63,6 +76,16 @@ public class HeadCommand extends ArgsOnlyCommand {
     return USAGE;
   }
 
+  private PathFilter getPartitionFilesFilter() {
+    return new PathFilter() {
+      @Override
+      public boolean accept(Path path) {
+        // ignore Spark '_SUCCESS' files
+        return !path.getName().equals("_SUCCESS");
+      }
+    };
+  }
+
   @Override
   public void execute(CommandLine options) throws Exception {
     super.execute(options);
@@ -76,11 +99,35 @@ public class HeadCommand extends ArgsOnlyCommand {
     String input = args[0];
 
     ParquetReader<SimpleRecord> reader = null;
+    boolean asJson = options.hasOption('j');
     try {
       PrintWriter writer = new PrintWriter(Main.out, true);
       reader = ParquetReader.builder(new SimpleReadSupport(), new Path(input)).build();
+      // we read footer only when output as json
+      JsonRecordFormatter.JsonGroupFormatter formatter = null;
+      if (asJson) {
+        // if input file is a directory search for any metadata
+        Configuration conf = new Configuration();
+        Path basePath = new Path(input);
+        FileSystem fs = basePath.getFileSystem(conf);
+        List<Footer> footers = ParquetFileReader.readAllFootersInParallelUsingSummaryFiles(
+          conf, Arrays.asList(fs.listStatus(basePath, getPartitionFilesFilter())));
+        // select first metadata that we found, fail if none available
+        if (footers == null || footers.isEmpty()) {
+          throw new IllegalArgumentException("Could not find metadata in " + basePath);
+        } else {
+          // footer.get(0) returns ParquetMetadata
+          ParquetMetadata metadata = footers.get(0).getParquetMetadata();
+          formatter = JsonRecordFormatter.fromSchema(metadata.getFileMetaData().getSchema());
+        }
+      }
+
       for (SimpleRecord value = reader.read(); value != null && num-- > 0; value = reader.read()) {
-        value.prettyPrint(writer);
+        if (asJson) {
+          writer.write(formatter.formatRecord(value));
+        } else {
+          value.prettyPrint(writer);
+        }
         writer.println();
       }
     } finally {
